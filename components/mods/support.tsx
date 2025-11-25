9e652c303c8057fc9a8542c33192f775bad179e2
Husanbek <buxorojahon@gmail.com>
2025-11-25 23:18:23 +0500

    W

--- components/mods/support.tsx
+++ components/mods/support.tsx
@@ -2,8 +2,16 @@
 export const Support = () => {
   return (
     <div className={`h-[140vh] bg-transparent py-20 flex items-center justify-center`}>
-        <div className="w-[75%] rounded-[50px] h-full bg-white">
-
+        <div className="w-[75%] rounded-[50px] h-full bg-white p-12">
+          <h1 className="uppercase text-gray-500 text-center font-semibold">Support</h1>
+           <h1 className="text-6xl font-bold text-[#222222] mt-4 text-center">
+              Give your audience <br />
+              an easy way to say thanks.
+          </h1>
+              <p className="text-2xl">
+             Buy Me a Coffee makes supporting fun and easy. In just a couple of taps, your fans <br /> 
+             can make the payment (buy you a coffee) and leave a message.
+          </p>
         </div>
     </div>
   )
